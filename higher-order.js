41f8c38d12007b1f409da03fc2ce9b1e716b5754
Lauren Irish <lirish2@gmail.com>
2019-03-01 15:45:52 -0700

    updated with answers except 6

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter((item, index, arr) => item % 2 == 0)
 
 
 
@@ -44,7 +43,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map((item, index, arr) => {
+  return item * 1.07; 
+});
 
 
 
@@ -63,7 +64,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((total, current, index, arr) => {
+  return total + current;
+})
 
 
 
@@ -89,7 +92,12 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((element, index, arr) => {
+  if(element.CP > 200) {
+    return index;
+  }
+
+})
 
 
 
@@ -103,10 +111,13 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax. 
+  Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map((element, index, arr) => {
+  return (element.price + (element.price * element.tax));
+})
 
 
 
@@ -126,6 +137,10 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((total, current, index, arr) => {
+  console.log(current)
+  
+})
+
 
 
